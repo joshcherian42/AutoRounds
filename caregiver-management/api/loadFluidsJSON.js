d36598144819f4190670f54b7cde9137d5617fbd
sjray32 <sjray32@gmail.com>
2023-02-10 10:31:53 -0600

    Switched other functions to next.js style syntax

--- caregiver-management/api/loadFluidsJSON.js
+++ caregiver-management/api/loadFluidsJSON.js
@@ -1,10 +1,22 @@
-import { readFileSync } from 'fs';
+// import { readFileSync } from 'fs';
+// import path from 'path';
+
+// export default function handler(req, res) {
+//   const file = path.join(process.cwd(), 'data', 'fluids.json');
+//   const stringified = readFileSync(file, 'utf8');
+
+//   res.setHeader('Content-Type', 'application/json');
+//   return res.end(stringified);
+// }
+
 import path from 'path';
+import { promises as fs } from 'fs';
 
-export default function handler(req, res) {
-  const file = path.join(process.cwd(), 'data', 'fluids.json');
-  const stringified = readFileSync(file, 'utf8');
-
-  res.setHeader('Content-Type', 'application/json');
-  return res.end(stringified);
+export default async function handler(req, res) {
+  //Find the absolute path of the json directory
+  const jsonDirectory = path.join(process.cwd(), 'data');
+  //Read the json data file data.json
+  const fileContents = await fs.readFile(jsonDirectory + '/fluids.json', 'utf8');
+  //Return the content of the data file in json format
+  res.status(200).json(fileContents);
 }
