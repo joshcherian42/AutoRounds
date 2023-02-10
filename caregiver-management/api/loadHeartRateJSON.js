6f743210934fd34d9d80608db0aff5f96eca0fbf
sjray32 <sjray32@gmail.com>
2023-02-10 10:50:48 -0600

    Revert to readFileSync and update vercel.json

--- caregiver-management/api/loadHeartRateJSON.js
+++ caregiver-management/api/loadHeartRateJSON.js
@@ -1,22 +1,22 @@
-// import { readFileSync } from 'fs';
-// import path from 'path';
-
-// export default function handler(req, res) {
-//   const file = path.join(process.cwd(), 'data', 'heart_rate.json');
-//   const stringified = readFileSync(file, 'utf8');
-
-//   res.setHeader('Content-Type', 'application/json');
-//   return res.end(stringified);
-// }
-
+import { readFileSync } from 'fs';
 import path from 'path';
-import { promises as fs } from 'fs';
 
-export default async function handler(req, res) {
-  //Find the absolute path of the json directory
-  const jsonDirectory = path.join('data');
-  //Read the json data file data.json
-  const fileContents = await fs.readFile(jsonDirectory + '/heart_rate.json', 'utf8');
-  //Return the content of the data file in json format
-  res.status(200).json(fileContents);
+export default function handler(req, res) {
+  const file = path.join(process.cwd(), 'data', 'heart_rate.json');
+  const stringified = readFileSync(file, 'utf8');
+
+  res.setHeader('Content-Type', 'application/json');
+  return res.end(stringified);
 }
+
+// import path from 'path';
+// import { promises as fs } from 'fs';
+
+// export default async function handler(req, res) {
+//   //Find the absolute path of the json directory
+//   const jsonDirectory = path.join('data');
+//   //Read the json data file data.json
+//   const fileContents = await fs.readFile(jsonDirectory + '/heart_rate.json', 'utf8');
+//   //Return the content of the data file in json format
+//   res.status(200).json(fileContents);
+// }
