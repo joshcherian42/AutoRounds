394b12e5ae49bb49324a9a97b5168101082caff9
sjray32 <sjray32@gmail.com>
2023-02-10 10:34:33 -0600

    Removing process.cwd()

--- caregiver-management/api/loadGlucoseJSON.js
+++ caregiver-management/api/loadGlucoseJSON.js
@@ -14,7 +14,7 @@ import { promises as fs } from 'fs';
 
 export default async function handler(req, res) {
   //Find the absolute path of the json directory
-  const jsonDirectory = path.join(process.cwd(), 'data');
+  const jsonDirectory = path.join('data');
   //Read the json data file data.json
   const fileContents = await fs.readFile(jsonDirectory + '/glucose.json', 'utf8');
   //Return the content of the data file in json format
