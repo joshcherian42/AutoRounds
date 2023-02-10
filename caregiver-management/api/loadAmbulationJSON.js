cf1b35333abfcb5510a2ea33e834d9b64c8e5163
sjray32 <sjray32@gmail.com>
2023-02-10 14:46:16 -0600

    Switching from return res.end to res.send

--- caregiver-management/api/loadAmbulationJSON.js
+++ caregiver-management/api/loadAmbulationJSON.js
@@ -12,17 +12,19 @@ export default function handler(req, res) {
   );
   const stringified = readFileSync(file, "utf8");
   // const stringified = await readFile(file, "utf8");
-  console.log("Got:", stringified);
+  // console.log("Got:", stringified);
 
   //const stringified =
   //  '{"data": [{"fk_resident_id": 1, "start_time": "2022-01-01T16:57:00", "end_time": "2022-01-01T17:09:00", "activity_type": "light"}, {"fk_resident_id": 1, "start_time": "2022-01-01T21:08:00", "end_time": "2022-01-01T21:53:00", "activity_type": "light"}]}';
 
   const data = JSON.parse(stringified)["data"];
-  console.log("Data:", data);
+  // console.log("Data:", data);
+  console.log("Got data");
   const filtered_data = data.filter(
     (row) => row["fk_resident_id"] === parseInt(resident_id)
   );
 
   res.setHeader("Content-Type", "application/json");
-  return res.end(filtered_data);
+  res.send(filtered_data);
+  // return res.end(filtered_data);
 }
